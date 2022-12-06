46280fa0c90b6b5d5a648afab185e243c8f99665
ahmetsedr <115935249+ahmetsedr@users.noreply.github.com>
2022-12-06 15:09:22 +0300

    HRD3v1.1

---
+++ a.js
@@ -0,0 +1,34 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    const N = parseInt(readLine().trim(), 10);
+
+    if ((N % 2 == 1) || (N % 2 == 0) && (N >= 6) && (N <= 20)) { // || Kullanırsak  Daha Temiz Kod Yazılmış Olursunuz. ve daha az satır kod yazmış olursunuz (program daha hızlı çalışır) 
+        console.log("Weird");
+    }
+    else if ((N % 2 == 0) && (2 <= N) && (N <= 5) || ((N % 2 == 0) && (N > 20))) {
+        console.log("Not Weird");
+    }
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 # Bu Bir patika.dev ödevidir.
+
+|| Kullanırsak  Daha Temiz Kod Yazılmış Olursunuz. ve daha az satır kod yazmış olursunuz (program daha hızlı çalışır) 
+
+## Gözden Kaçırdığım birşey varsa lütfen söyleyin
